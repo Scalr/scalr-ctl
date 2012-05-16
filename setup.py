79d2f0bc52b88da94de32a16f946990b1eb56942
Dmitry Korsakov <dmitry@scalr.com>
2012-05-16 13:41:35 +0000

    fixed for setup.py and readme file

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ Created on Feb 21th, 2011
 
 @author: Dmytro Korsakov
 '''
-
+import os
 from setuptools import setup, find_packages
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 description = "Scalr is a command-line interface to your Scalr account"
 
@@ -13,7 +15,6 @@ cfg = dict(
 	name = "scalr",
 	version = "0.3.19",	 
 	description = description,
-	long_description = description,
 	author = "Scalr Inc.",
 	author_email = "dmitry@scalr.com",
 	url = "https://scalr.net",
