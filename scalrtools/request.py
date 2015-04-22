f10f47ef41e6ed012cfe150f7fe402aef96e033a
Dmytro Korsakov <dmitry@scalr.com>
2015-04-22 15:39:05 +0300

    debug disabled

--- scalrtools/request.py
+++ scalrtools/request.py
@@ -20,22 +20,24 @@ import logging
 # these two lines enable debugging at httplib level (requests->urllib3->httplib)
 # you will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
 # the only thing missing will be the response.body which is not logged.
+"""
 import httplib
 httplib.HTTPConnection.debuglevel = 1
+
 
 logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
 logging.getLogger().setLevel(logging.DEBUG)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
+"""
 
 
 
-
-#try:
-#requests.packages.urllib3.disable_warnings()
-#except:
-#    pass
+try:
+    requests.packages.urllib3.disable_warnings()
+except:
+    pass
 
 def request(method, request_uri, payload=None, data=None):
     scheme = settings.API_SCHEME
@@ -71,7 +73,10 @@ def request(method, request_uri, payload=None, data=None):
             click.echo("%s URL: %s" % (method.upper(), url))
             click.echo("Headers: %s " % json.dumps(headers, indent=2))
 
-        r = requests.request(method.lower(), url, data=body, params=payload, headers=headers, verify=True)
+        #session = requests.sessions.Session()
+        #session.headers.pop('Connection')
+        r = requests.request(method.lower(), url, data=body, params=payload, headers=headers)
+        #session.close()
         result = r.text
 
 
