0c25b5ee1932da6e8ecc3f2eae55c59f8b298a00
Dmytro Korsakov <dmitry@scalr.com>
2015-06-26 16:12:25 +0300

    ST-47

--- post_setup.py
+++ post_setup.py
@@ -16,7 +16,7 @@ def main():
 
     import click
 
-    confirmed = click.confirm("Modify profile to update your $PATH and enable bash completion?", default=True)
+    confirmed = click.confirm("Modify profile to update your $PATH and enable bash completion?", default=True, err=True)
 
     if confirmed:
         with open(AUTOCOMPLETE_PATH, "w") as fp:
@@ -25,7 +25,7 @@ def main():
         bashrc_path = os.path.expanduser("~/.bashrc")
         bashprofile_path = os.path.expanduser("~/.bash_profile")
         startup_path = bashprofile_path if os.path.exists(bashprofile_path) else bashrc_path
-        startup_path = click.prompt("Enter path to an rc file to update, or leave blank to use", default=startup_path)
+        startup_path = click.prompt("Enter path to an rc file to update, or leave blank to use", default=startup_path, err=True)
 
         backup_path = startup_path + ".backup"
         click.echo("Backing up [%s] to [%s]." % (startup_path, backup_path))
@@ -37,6 +37,12 @@ def main():
             comment = "# The next line enables bash completion for %s." % PROGNAME
             newline = "" if startupfile_content.endswith("\n") else "\n"
             add = "%s%s\n%s" % (newline, comment, source_line)
+
+            # Handling pip install --user and PATH
+            local_binpath = os.path.join(os.path.expanduser("~/.local/bin/"), PROGNAME)
+            if os.path.exists(local_binpath):
+                add += "\nasias %s=%s" % (PROGNAME, local_binpath)
+
             with open(startup_path, "a") as afp:
                 afp.write(add)
 
