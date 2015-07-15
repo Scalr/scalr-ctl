a10760aa4686e4e3c8b798b60eb6dd10f3d3c9b8
Dmytro Korsakov <dmitry@scalr.com>
2015-07-15 14:39:52 +0300

    postinstall script moved to 'configure' command

--- post_setup.py
+++ post_setup.py
@@ -1,53 +1,8 @@
 __author__ = 'Dmitriy Korsakov'
 
 import os
-import shutil
 
 
-PROGNAME = "scalr-tools"
-DEFAULT_CONFIGDIR = "~/.scalr"
-AUTOCOMPLETE_FNAME = "path.bash.inc"
-AUTOCOMPLETE_CONTENT = "_%s_COMPLETE=source %s" % (PROGNAME.upper().replace("-", "_"), PROGNAME)
-AUTOCOMPLETE_PATH = os.path.join(os.path.expanduser(DEFAULT_CONFIGDIR), AUTOCOMPLETE_FNAME)
 
 def main():
-    if "nt" == os.name: # Click currently only supports completion for Bash.
-        return
-
-    import click
-
-    confirmed = click.confirm("Modify profile to update your $PATH and enable bash completion?", default=True, err=True)
-
-    if confirmed:
-        with open(AUTOCOMPLETE_PATH, "w") as fp:
-            fp.write(AUTOCOMPLETE_CONTENT)
-
-        bashrc_path = os.path.expanduser("~/.bashrc")
-        bashprofile_path = os.path.expanduser("~/.bash_profile")
-        startup_path = bashprofile_path if os.path.exists(bashprofile_path) else bashrc_path
-        startup_path = click.prompt("Enter path to an rc file to update, or leave blank to use", default=startup_path, err=True)
-
-        backup_path = startup_path + ".backup"
-        click.echo("Backing up [%s] to [%s]." % (startup_path, backup_path))
-        shutil.copy(startup_path, backup_path)
-
-        source_line = "source '%s'" % AUTOCOMPLETE_PATH
-        startupfile_content = open(startup_path, "r").read()
-        if AUTOCOMPLETE_PATH not in startupfile_content:
-            comment = "# The next line enables bash completion for %s." % PROGNAME
-            newline = "" if startupfile_content.endswith("\n") else "\n"
-            add = "%s%s\n%s" % (newline, comment, source_line)
-
-            # Handling pip install --user and PATH
-            local_binpath = os.path.join(os.path.expanduser("~/.local/bin/"), PROGNAME)
-            if os.path.exists(local_binpath):
-                add += "\nasias %s=%s" % (PROGNAME, local_binpath)
-
-            with open(startup_path, "a") as afp:
-                afp.write(add)
-
-        click.echo("Start a new shell for the changes to take effect.")
-
-
-if __name__ == '__main__':
-    main()
+    pass
--- scalrctl/app.py
+++ scalrctl/app.py
@@ -3,6 +3,7 @@ __author__ = 'Dmitriy Korsakov'
 import os
 import sys
 import json
+import shutil
 import inspect
 
 import yaml
@@ -14,14 +15,21 @@ import settings
 import spec
 
 
+PROGNAME = "scalr-ctl"
 DEFAULT_PROFILE = "default"
 CMD_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'commands'))
 CONFIG_FOLDER = os.path.expanduser(os.environ.get("SCALRCLI_HOME", "~/.scalr"))
 CONFIG_PATH = os.path.join(CONFIG_FOLDER, "%s.yaml" % os.environ.get("SCALRCLI_PROFILE", DEFAULT_PROFILE))
+
 SWAGGER_FILE = "user.yaml"
 SWAGGER_PATH = os.path.join(CONFIG_FOLDER, SWAGGER_FILE)
 SWAGGER_JSONSPEC_FILE = SWAGGER_FILE.split(".")[0] + ".json"
 SWAGGER_JSONSPEC_PATH = os.path.join(CONFIG_FOLDER, SWAGGER_JSONSPEC_FILE)
+
+AUTOCOMPLETE_FNAME = "path.bash.inc"
+AUTOCOMPLETE_CONTENT = "_%s_COMPLETE=source %s" % (PROGNAME.upper().replace("-", "_"), PROGNAME)
+AUTOCOMPLETE_PATH = os.path.join(os.path.expanduser(CONFIG_FOLDER), AUTOCOMPLETE_FNAME)
+
 
 def use(profile=None):
     """
@@ -49,6 +57,45 @@ def use(profile=None):
         errmsg = "Cannot switch profile: %s not found. Available profiles: [%s]. " % (path, ", ".join(list_profiles))
         errmsg += "Use 'configure' command to create new profiles."
         raise click.ClickException(errmsg)
+
+
+def setup_bash_complete():
+    if "nt" == os.name: # Click currently only supports completion for Bash.
+        return
+
+    import click
+
+    confirmed = click.confirm("Modify profile to update your $PATH and enable bash completion?", default=True, err=True)
+
+    if confirmed:
+        with open(AUTOCOMPLETE_PATH, "w") as fp:
+            fp.write(AUTOCOMPLETE_CONTENT)
+
+        bashrc_path = os.path.expanduser("~/.bashrc")
+        bashprofile_path = os.path.expanduser("~/.bash_profile")
+        startup_path = bashprofile_path if os.path.exists(bashprofile_path) else bashrc_path
+        startup_path = click.prompt("Enter path to an rc file to update, or leave blank to use", default=startup_path, err=True)
+
+        backup_path = startup_path + ".backup"
+        click.echo("Backing up [%s] to [%s]." % (startup_path, backup_path))
+        shutil.copy(startup_path, backup_path)
+
+        source_line = "source '%s'" % AUTOCOMPLETE_PATH
+        startupfile_content = open(startup_path, "r").read()
+        if AUTOCOMPLETE_PATH not in startupfile_content:
+            comment = "# The next line enables bash completion for %s." % PROGNAME
+            newline = "" if startupfile_content.endswith("\n") else "\n"
+            add = "%s%s\n%s" % (newline, comment, source_line)
+
+            # Handling pip install --user and PATH
+            local_binpath = os.path.join(os.path.expanduser("~/.local/bin/"), PROGNAME)
+            if os.path.exists(local_binpath):
+                add += "\nasias %s=%s" % (PROGNAME, local_binpath)
+
+            with open(startup_path, "a") as afp:
+                afp.write(add)
+
+        click.echo("Start a new shell for the changes to take effect.")
 
 
 def configure(profile=None):
@@ -88,6 +135,7 @@ def configure(profile=None):
     click.echo(open(confpath, "r").read())
 
     update()
+    setup_bash_complete()
 
 
 def update():
