423bcea7a47583a41c1c1a02925dfbf894f6219a
Dmitiy Korsakov <shaitanich@gmail.com>
2016-04-19 19:18:09 +0300

    ST-88

--- scalrctl/commands/import.py
+++ scalrctl/commands/import.py
@@ -27,7 +27,7 @@ class Import(commands.Action):
     def get_options(self):
         debug = click.Option(('--debug/--no-debug', 'debug'), default=False, help="Print debug messages")
         upd_helpmsg = "Update existing object instead of creating new."
-        update = click.Option(('--update', 'update'), default=False, help=upd_helpmsg)
+        update = click.Option(('--update', 'update'), is_flag=True, default=False, help=upd_helpmsg)
         return [debug, update]
 
     def run(self, *args, **kwargs):
@@ -38,18 +38,21 @@ class Import(commands.Action):
         obj_data = self._validate_object(raw)
 
         get_type_route = obj_data["meta"]["scalrctl"]["ROUTE"]
-        if "id" in obj_data and obj_data["id"]:
-            http_method = "patch"
-            route = get_type_route
-        else:
-            http_method = "post"
-            route = self._get_patch_type_route(get_type_route=get_type_route)
 
-        print route, self._get_patch_type_route(get_type_route=get_type_route)
-        return
+        if "update" in kwargs:
+            update = kwargs.pop("update")
+
+            if update:
+                http_method = "patch"
+                route = get_type_route
+            else:
+                http_method = "post"
+                route = self._get_post_type_route(self.scheme, get_type_route=get_type_route)
+                print "GET route: %s,POST route: %s" % (get_type_route, route)
 
         if not route:
-            raise click.ClickException("Cannot import Scalr object: API method (%s,%s) not found" % (http_method, route))
+            raise click.ClickException("Cannot import Scalr object: API method (%s,%s) not found" % (
+                http_method, route))
 
         action_scheme = self._lookup_action_scheme(self.scheme, route, http_method)
 
@@ -60,26 +63,37 @@ class Import(commands.Action):
 
         self.action = cls(name=self.name, route=route, http_method=http_method, api_level=self.api_level)
 
-        """
-        # TODO: 1. Get ID name 2. Search for ID in user input 3. POST if ID is empty else PATCH
-        """
+        arguments = obj_data["meta"]["scalrctl"]['ARGUMENTS']  # XXX
+        # self.action.run(*arguments[0], **arguments[1])
 
-        """
-        self.api_level = obj["meta"]["scalrctl"]["API_LEVEL"]
-        self.http_method = obj["meta"]["scalrctl"]["METHOD"]
-        self.
-        """
-
-        arguments = obj_data["meta"]["scalrctl"]['arguments']  # XXX
-        self.action.run(*arguments[0], **arguments[1])
-
-    def _get_patch_type_route(self, get_type_route):
+    def _get_post_type_route(self, dct, get_type_route):
         """
         :param get_type_route: route of GET-method for Scalr object
         :returns route of POST-method for Scalr object
         Example: _get_patch_type_route("/images/{imageId}/") returns "/images/"
         """
-        return get_type_route  # XXX
+        found = False
+        patch_route = None
+        for k, v in dct.items():
+            is_action = "class" in v
+            is_object = "group_descr" in v
+            is_level = not is_action and not is_object
+            if is_action:
+                continue
+            elif is_object:
+                for action, block in v.items():
+                    if "route" in block and "http-method" in block:
+                        if block["http-method"] == "get" and block["route"] == get_type_route:
+                            found = True
+                            continue
+                        elif block["http-method"] == "post" and get_type_route.startswith(block["route"]):
+                            patch_route = block["route"]
+            if found:
+                return patch_route
+            elif is_level:
+                patch_route = self._get_post_type_route(v, get_type_route)
+                if patch_route:
+                    return patch_route
 
     def _lookup_action_scheme(self, dct, route, http_method):
         if "route" in dct and dct["route"] == route and dct["http-method"] == http_method:
@@ -93,7 +107,6 @@ class Import(commands.Action):
     def _validate_object(self, yml):
         try:
             descr = yaml.load(yml)
-            print "YAML:", descr
             assert "data" in descr
             assert "meta" in descr
             meta = descr["meta"]
