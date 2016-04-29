6211ca36cce201d9b865f0bd59c0f901ab2f240d
Dmitiy Korsakov <shaitanich@gmail.com>
2016-04-29 14:36:57 +0300

    ST-121

--- scalrctl/defaults.py
+++ scalrctl/defaults.py
@@ -4,4 +4,4 @@ import os
 
 CONFIG_DIRECTORY = os.path.expanduser(os.environ.get("SCALRCLI_HOME", os.path.join(os.path.expanduser("~"), ".scalr")))
 CONFIG_PATH = os.path.join(CONFIG_DIRECTORY, "%s.yaml" % os.environ.get("SCALRCLI_PROFILE", "default"))
-VERSION = open(os.path.join(os.path.dirname(__file__), "../VERSION")).read().strip()
+VERSION = open(os.path.join(os.path.dirname(__file__), "VERSION")).read().strip()
