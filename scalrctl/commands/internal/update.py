e34d7f74ef4c40cc88f484cf9642e4665710cc35
Dmitiy Korsakov <shaitanich@gmail.com>
2016-06-01 18:59:46 +0300

    ST-126 update for global scope

--- scalrctl/commands/internal/update.py
+++ scalrctl/commands/internal/update.py
@@ -14,17 +14,8 @@ from scalrctl import defaults
 from scalrctl import settings
 from scalrctl import commands
 
-SWAGGER_USER_NOUPDATE_TRIGGER = ".noupdate.user"
-SWAGGER_USER_FILE = "user.yaml"
-SWAGGER_USER_PATH = os.path.join(defaults.CONFIG_DIRECTORY, SWAGGER_USER_FILE)
-SWAGGER_USER_JSONSPEC_FILE = SWAGGER_USER_FILE.split(".")[0] + ".json"
-SWAGGER_USER_JSONSPEC_PATH = os.path.join(defaults.CONFIG_DIRECTORY, SWAGGER_USER_JSONSPEC_FILE)
 
-SWAGGER_ACCOUNT_NOUPDATE_TRIGGER = ".noupdate.account"
-SWAGGER_ACCOUNT_FILE = "account.yaml"
-SWAGGER_ACCOUNT_PATH = os.path.join(defaults.CONFIG_DIRECTORY, SWAGGER_ACCOUNT_FILE)
-SWAGGER_ACCOUNT_JSONSPEC_FILE = SWAGGER_ACCOUNT_FILE.split(".")[0] + ".json"
-SWAGGER_ACCOUNT_JSONSPEC_PATH = os.path.join(defaults.CONFIG_DIRECTORY, SWAGGER_ACCOUNT_JSONSPEC_FILE)
+successful = False
 
 
 class UpdateScalrCTL(commands.BaseAction):
@@ -37,29 +28,75 @@ class UpdateScalrCTL(commands.BaseAction):
 
 
 def is_update_required():
-    return not os.path.exists(SWAGGER_USER_PATH) or not os.path.exists(SWAGGER_USER_JSONSPEC_PATH)
+    return not os.path.exists(get_yamlspec_path("user")) \
+           or not os.path.exists(get_jsonspec_path("user")) \
+           or not os.path.exists(get_yamlspec_path("account")) \
+           or not os.path.exists(get_jsonspec_path("account")) \
+           or not os.path.exists(get_yamlspec_path("global")) \
+           or not os.path.exists(get_jsonspec_path("global"))
 
 
 def get_spec_url(api_level="user"):
-    api_level = api_level or "user"  #XXX: Ugly,  SubCommand class needs to be changed first
+    api_level = api_level or "user"
     return "{0}://{1}/api/{2}.{3}.yml".format(settings.API_SCHEME, settings.API_HOST, api_level, settings.API_VERSION)
+
+
+def get_trigger_path(api_level="user"):
+    fname = ".noupdate.%s" % api_level
+    return os.path.join(defaults.CONFIG_DIRECTORY, fname)
+
+
+def get_yamlspec_path(api_level="user"):
+    fname = "%s.yaml" % api_level
+    return os.path.join(defaults.CONFIG_DIRECTORY, fname)
+
+
+def get_jsonspec_path(api_level="user"):
+    fname = "%s.json" % api_level
+    return os.path.join(defaults.CONFIG_DIRECTORY, fname)
+
+
+def update_spec(api_level="user"):
+    global successful
+    paths = []
+    text = None
+
+    trigger_path = get_trigger_path(api_level=api_level)
+    url = get_spec_url(api_level=api_level)
+    yaml_dst = get_yamlspec_path(api_level=api_level)
+    json_dst = get_jsonspec_path(api_level=api_level)
+
+    if not os.path.exists(trigger_path):
+        r = requests.get(url)
+
+        old = None
+        if os.path.exists(yaml_dst):
+            with open(yaml_dst, "r") as fp:
+                old = fp.read()
+
+        text = r.text
+
+        if text == old:
+            successful = True
+
+        elif text:
+            with open(yaml_dst, "w") as fp:
+                fp.write(text)
+            successful = True
+
+    if text or os.path.exists(yaml_dst):
+        struct = yaml.load(text or open(yaml_dst).read())
+        paths = struct["paths"].keys()
+        json.dump(struct, open(json_dst, "w"))
+
+    return paths
 
 
 def update():
     """
     Downloads yaml spec and converts it to JSON
-    Both files are stored in configuration directory.
+    Files are stored in configuration directory.
     """
-    account_paths = []
-    user_paths = []
-
-    successfull = False
-    text = None
-    user_trigger_file = os.path.join(defaults.CONFIG_DIRECTORY, SWAGGER_USER_NOUPDATE_TRIGGER)
-
-    user_url = get_spec_url(api_level="user")
-    user_dst = os.path.join(defaults.CONFIG_DIRECTORY, SWAGGER_USER_FILE)
-
     def spinning_cursor():
         while True:
             for cursor in '|/-\\':
@@ -82,72 +119,15 @@ def update():
     t.start()
 
     try:
-        if user_url and not os.path.exists(user_trigger_file):
-            # click.echo("Trying to get new UserAPI Spec from %s" % user_url)
-            r = requests.get(user_url)
-
-            old = None
-
-            if os.path.exists(user_dst):
-                with open(user_dst, "r") as fp:
-                    old = fp.read()
-
-            text = r.text
-
-            if text == old:
-                # click.echo("UserAPI Spec is already up-to-date.")
-                successfull = True
-            elif text:
-                with open(user_dst, "w") as fp:
-                    fp.write(text)
-                # click.echo("UserAPI UserSpec successfully updated.")
-                successfull = True
-
-        if text or os.path.exists(user_dst):
-            struct = yaml.load(text or open(user_dst).read())
-            user_paths = struct["paths"].keys()
-            json.dump(struct, open(SWAGGER_USER_JSONSPEC_PATH, "w"))
-
-
-        # Fetch AccountAPI spec and convert to JSON
-        text = None
-        account_trigger_file = os.path.join(defaults.CONFIG_DIRECTORY, SWAGGER_ACCOUNT_NOUPDATE_TRIGGER)
-        account_url = get_spec_url(api_level="account")
-        account_dst = os.path.join(defaults.CONFIG_DIRECTORY, SWAGGER_ACCOUNT_FILE)
-
-        if account_url and not os.path.exists(account_trigger_file):
-            # click.echo("Trying to get new AccountAPI Spec from %s" % account_url)
-            r = requests.get(account_url)
-
-            old = None
-
-            if os.path.exists(account_dst):
-                with open(account_dst, "r") as fp:
-                    old = fp.read()
-
-            text = r.text
-
-            if text == old:
-                # click.echo("AccountAPI Spec is already up-to-date.")
-                successfull = True
-            elif text:
-                with open(account_dst, "w") as fp:
-                    fp.write(text)
-                # click.echo("AccountAPI Spec successfully updated.")
-                successfull = True
-
-        if text or os.path.exists(account_dst):
-            struct = yaml.load(text or open(account_dst).read())
-            account_paths = struct["paths"].keys()
-            json.dump(struct, open(SWAGGER_ACCOUNT_JSONSPEC_PATH, "w"))
-
-        available_routes = {"user": user_paths, "account": account_paths}
+        available_routes = {}
+        for api_level in ("user", "account", "global"):
+            available_routes[api_level] = update_spec(api_level=api_level)
         json.dump(available_routes, open(defaults.ROUTES_PATH, "w"))
 
     finally:
         e.set()
         t.join()
-        if successfull:
+        if successful:
             click.echo("Done")
         else:
             click.echo("Failed")
