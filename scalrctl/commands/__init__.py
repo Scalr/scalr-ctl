47e24da2b8efd9ebed7ddbc8ccbfc744a0101aec
Dmitiy Korsakov <shaitanich@gmail.com>
2016-07-27 20:45:54 +0300

    ST-163

--- scalrctl/commands/__init__.py
+++ scalrctl/commands/__init__.py
@@ -301,7 +301,7 @@ class Action(BaseAction):
 
     def _get_custom_options(self):
         options = []
-        debug = click.Option(('--debug/--no-debug', 'debug'), default=False, help="Print debug messages")
+        debug = click.Option(('--debug', 'debug'), default=False, help="Print debug messages")
         options.append(debug)
 
         if self.http_method.upper() == 'GET':
