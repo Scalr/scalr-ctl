f2a412e0efca22b4b9af97ead6241ff8fa6938a3
Sergey Babak <s.babak@scalr.com>
2016-08-31 13:48:15 +0300

    ST-178 Regression tests for every command's help

--- scalrctl/defaults.py
+++ scalrctl/defaults.py
@@ -6,6 +6,7 @@ CONFIG_DIRECTORY = os.path.expanduser(os.environ.get("SCALRCLI_HOME", os.path.jo
 CONFIG_PATH = os.path.join(CONFIG_DIRECTORY, "%s.yaml" % os.environ.get("SCALRCLI_PROFILE", "default"))
 VERSION = open(os.path.join(os.path.dirname(__file__), "VERSION")).read().strip()
 ROUTES_PATH = os.path.join(CONFIG_DIRECTORY, "available_api_routes.json")
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 API_LEVELS = [
     'user',
