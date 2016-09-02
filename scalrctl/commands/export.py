c19c0c0a96c22011e11d5f9f0aba78e1f2afef5a
Sergey Babak <s.babak@scalr.com>
2016-09-02 18:35:06 +0300

    ST-134 Import/export for Roles

--- scalrctl/commands/export.py
+++ scalrctl/commands/export.py
@@ -1,15 +1,16 @@
-__author__ = 'Dmitriy Korsakov'
-
-import json
-import datetime
+"""
+Export Scalr objects.
+"""
 import copy
-
-from scalrctl import settings
-from scalrctl import commands
-from scalrctl import click
-from scalrctl import defaults
+import datetime
+import json
 
 import yaml
+
+from scalrctl import click, commands, defaults, settings
+
+
+__author__ = 'Dmitriy Korsakov'
 
 
 class Export(commands.Action):
@@ -17,41 +18,38 @@ class Export(commands.Action):
     def _get_custom_options(self):
         # Disable output modifiers
         options = []
-        debug = click.Option(
-            ('--debug', 'debug'),
-            is_flag=True,
-            default=False,
-            help="Print debug messages")
+        debug = click.Option(('--debug', 'debug'), is_flag=True,
+                             default=False, help="Print debug messages")
         options.append(debug)
         return options
 
     def run(self, *args, **kwargs):
-        hide_output = kwargs.pop("hide_output", False)
+        hide_output = kwargs.pop('hide_output', False)
 
         kv = kwargs.copy()
         kv['hide_output'] = True
         response = super(Export, self).run(*args, **kv)
 
-        kwargs["envId"] = settings.envId
+        kwargs['envId'] = settings.envId
         kv = copy.deepcopy(kwargs)
-        for item in ("debug", "nocolor", "transformation"):
+        for item in ('debug', 'nocolor', 'transformation'):
             if item in kv:
                 del kv[item]
         uri = self._request_template.format(**kwargs)
 
-        d = {
-            "API_VERSION": settings.API_VERSION,
-            "envId": settings.envId,
-            "DATE": datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
-            "API_HOST": settings.API_HOST,
-            "API_LEVEL": self.api_level,
-            "METHOD": self.http_method,
-            "ROUTE": self.route,
-            "URI": uri,
-            "ACTION": self.name,
-            "ARGUMENTS": (args, kv),
-            "SCALRCTL_VERSION": defaults.VERSION,
-             }
+        scalrctl_meta = {
+            'API_VERSION': settings.API_VERSION,
+            'envId': settings.envId,
+            'DATE': datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S'),
+            'API_HOST': settings.API_HOST,
+            'API_LEVEL': self.api_level,
+            'METHOD': self.http_method,
+            'ROUTE': self.route,
+            'URI': uri,
+            'ACTION': self.name,
+            'ARGUMENTS': (args, kv),
+            'SCALRCTL_VERSION': defaults.VERSION,
+        }
 
         try:
             response_json = json.loads(response)
@@ -60,65 +58,73 @@ class Export(commands.Action):
                 raise
             raise click.ClickException(str(e))
 
-        response_json["meta"]["scalrctl"] = d
+        data = {
+            'meta': {
+                'scalrctl': scalrctl_meta
+            },
+            'include': [
+                response_json,
+            ]
+        }
 
-        dump = yaml.safe_dump(
-            response_json,
-            encoding='utf-8',
-            allow_unicode=True,
-            default_flow_style=False
-        )
         if not hide_output:
+            dump = yaml.safe_dump(
+                data, encoding='utf-8',
+                allow_unicode=True,
+                default_flow_style=False
+            )
             click.echo(dump)
-        return response_json
+        return data
 
 
 class ExportFarmRoleGlobalVariable(Export):
-    prompt_for = ["roleId", "globalVariableName"]
+    prompt_for = ['roleId', 'globalVariableName']
 
 
 class ExportImage(Export):
-    prompt_for = ["imageId"]
+    prompt_for = ['imageId']
 
 
 class ExportScript(Export):
 
     def run(self, *args, **kwargs):
         # TODO: help for update! (some script-versions may be deleted)
-        # interactive update by default (prompt for deletes and updates), with optional force
-        kwargs["hide_output"] = True
+        # interactive update by default (prompt for deletes and updates),
+        # with optional force
+        kwargs['hide_output'] = True
         response = super(ExportScript, self).run(*args, **kwargs)
 
-        svlist_action = commands.Action(
-            "script-version",
-            "/{envId}/scripts/{scriptId}/script-versions/",
-            "get",
-            "user"
+        sv_list_action = commands.Action(
+            'script-version',
+            '/{envId}/scripts/{scriptId}/script-versions/',
+            'get',
+            'user'
         )
-        svlist_text = svlist_action.run(envId=kwargs["envId"], scriptId=kwargs["scriptId"], hide_output=True)
-        svlist_dict = json.loads(svlist_text)
-        svlist = svlist_dict["data"]
+        sv_list_resp = sv_list_action.run(
+            envId=kwargs['envId'],
+            scriptId=kwargs['scriptId'],
+            hide_output=True
+        )
+        sv_list_json = json.loads(sv_list_resp)
 
-        svget_action = Export(
-            "script-version",
-            "/{envId}/scripts/{scriptId}/script-versions/{scriptVersionNumber}/",
-            "get",
-            "user"
+        sv_get_action = Export(
+            'script-version',
+            '/{envId}/scripts/{scriptId}/'
+            'script-versions/{scriptVersionNumber}/',
+            'get',
+            'user',
         )
-        include = []
-        for sv in svlist:
-            script_version = sv["version"]
-            sctipt_body = sv["body"]
-            if sctipt_body:
-                out = svget_action.run(
-                    envId=kwargs["envId"],
-                    scriptId=kwargs["scriptId"],
-                    scriptVersionNumber=script_version,
+
+        for script in sv_list_json['data']:
+            if script.get('body'):
+                resp = sv_get_action.run(
+                    envId=kwargs['envId'],
+                    scriptId=kwargs['scriptId'],
+                    scriptVersionNumber=script['version'],
                     hide_output=True
                 )
-                include.append(out)
-
-        response["include"] = include
+                for item in resp['include']:
+                    response['include'].append(item)
 
         dump = yaml.safe_dump(
             response,
@@ -127,4 +133,6 @@ class ExportScript(Export):
             default_flow_style=False
         )
         click.echo(dump)
+
+        #click.echo(json.dumps(response, indent=2))
         return response
--- scalrctl/commands/import.py
+++ scalrctl/commands/import.py
@@ -1,14 +1,16 @@
-__author__ = 'Dmitriy Korsakov'
-
-import os
+"""
+Import Scalr objects.
+"""
 import json
+import os
 import pydoc
 
 import yaml
 
-from scalrctl import click
-from scalrctl import commands
-from scalrctl import settings
+from scalrctl import click, commands, settings
+
+
+__author__ = 'Dmitriy Korsakov'
 
 
 class Import(commands.Action):
@@ -19,81 +21,87 @@ class Import(commands.Action):
     def _init(self):
         super(Import, self)._init()
 
-        with open(os.path.join(os.path.dirname(__file__), '../scheme/scheme.json')) as fp:
+        with open(os.path.join(os.path.dirname(__file__),
+                               '../scheme/scheme.json')) as fp:
             self.scheme = json.load(fp)
 
     def get_description(self):
         return "Import Scalr Objects."
 
     def get_options(self):
-        debug = click.Option(('--debug', 'debug'), is_flag=True, default=False, help="Print debug messages")
+        debug = click.Option(('--debug', 'debug'), is_flag=True,
+                             default=False, help="Print debug messages")
         envid = click.Option(('--envId', 'env_id'), help="Environment ID")
         upd_helpmsg = "Update existing object instead of creating new."
-        update = click.Option(('--update', 'update'), is_flag=True, default=False, help=upd_helpmsg, hidden=True)
-        dry_run = click.Option(('--dryrun', 'dryrun'), is_flag=True, default=False, help=upd_helpmsg, hidden=True)
+        update = click.Option(('--update', 'update'), is_flag=True,
+                              default=False, help=upd_helpmsg, hidden=True)
+        dry_run = click.Option(('--dryrun', 'dryrun'), is_flag=True,
+                               default=False, help=upd_helpmsg, hidden=True)
         return [debug, update, envid, dry_run]
 
     def run(self, *args, **kwargs):
-        if "debug" in kwargs:
-            settings.debug_mode = kwargs.pop("debug")
-        if "dryrun" in kwargs:
-            dry_run_mode = kwargs.pop("dryrun")
+        if 'debug' in kwargs:
+            settings.debug_mode = kwargs.pop('debug')
+        if 'dryrun' in kwargs:
+            dry_run_mode = kwargs.pop('dryrun')
 
-        env_id = kwargs.pop("env_id", None) or settings.envId
-        raw = kwargs.pop("raw", None) or click.get_text_stream("stdin")
+        env_id = kwargs.pop('debug', None) or settings.envId
+        raw = kwargs.pop('raw', None) or click.get_text_stream('stdin')
         obj_data = self._validate_object(raw)
 
-        get_type_route = obj_data["meta"]["scalrctl"]["ROUTE"]
-        update_mode = kwargs.pop("update", False)
-        http_method = "patch" if update_mode else "post"
+        get_type_route = obj_data['meta']['scalrctl']['ROUTE']
+        update_mode = kwargs.pop('update', False)
+        http_method = 'patch' if update_mode else 'post'
 
         action_scheme = None
-        for obj_name, section in self.scheme["export"].items():
-            if obj_name != "group_descr" and section["http-method"] == "get" and section["route"] == get_type_route:
+        for obj_name, section in self.scheme['export'].items():
+            if 'http-method' in section and 'route' in section and \
+               section['http-method'] == 'get' and \
+               section['route'] == get_type_route:
                 action_scheme = section["%s-params" % http_method]
 
         if not action_scheme:
-            raise click.ClickException("Cannot import Scalr object: API method (%s,%s) not found" % (
-                action_scheme["http-method"],
-                action_scheme["route"]))
+            msg = "Cannot import Scalr object: API method '{}: {}' not found" \
+                .format('get'.upper(), get_type_route)
+            raise click.ClickException(msg)
 
-        cls = pydoc.locate(action_scheme['class']) if 'class' in action_scheme else commands.Action
-        action = cls(name=self.name, route=action_scheme['route'], http_method=http_method, api_level=self.api_level)
+        cls = pydoc.locate(
+            action_scheme['class']
+        ) if 'class' in action_scheme else commands.Action
+        action = cls(name=self.name, route=action_scheme['route'],
+                     http_method=http_method, api_level=self.api_level)
 
-        arguments, kv = obj_data["meta"]["scalrctl"]['ARGUMENTS']
+        arguments, kv = obj_data['meta']['scalrctl']['ARGUMENTS']
 
-        obj_type = action.get_body_type_params()[0]["name"]
-        kv["import-data"] = {obj_type: obj_data["data"]}
+        obj_type = action.get_body_type_params()[0]['name']
+        kv['import-data'] = {obj_type: obj_data['data']}
         if env_id:
-            kv["envId"] = env_id
+            kv['envId'] = env_id
         if dry_run_mode:
-            kv["dryrun"] = dry_run_mode
+            kv['dryrun'] = dry_run_mode
 
-        click.echo("\x1b[1m%s %s %s\x1b[0m" % (
+        click.secho("{} {} {}".format(
             "Updating" if update_mode else "Creating",
             self._get_object_alias(obj_type),
-            "ID" if update_mode else ""  # TBD: ID
-        ))
-        click.echo()
+            "ID" if update_mode else ""
+        ), bold=True)
 
         result = action.run(*arguments, **kv)
         result_json = json.loads(result)
 
-        if "include" in obj_data:
-            included_objects = obj_data["include"]
+        if 'include' in obj_data:
+            for obj in obj_data['include']:
 
-            for obj in included_objects:
-
-                if obj["meta"]["scalrctl"]["ACTION"] == "script-version":  # XXX
-                    obj["meta"]["scalrctl"]['ARGUMENTS'][1]['scriptId'] = result_json["data"]["id"]
+                if obj['meta']['scalrctl']['ACTION'] == 'script-version':  # XXX
+                    obj['meta']['scalrctl']['ARGUMENTS'][1]['scriptId'] = result_json['data']['id']
 
                 inc_raw = yaml.dump(obj)
                 inc_kv = {
-                    "debug": settings.debug_mode,
-                    "env_id": env_id,
-                    "dryrun": dry_run_mode,
-                    "update": update_mode,
-                    "raw": inc_raw,
+                    'debug': settings.debug_mode,
+                    'env_id': env_id,
+                    'dryrun': dry_run_mode,
+                    'update': update_mode,
+                    'raw': inc_raw,
                 }
 
                 self.run(**inc_kv)
@@ -106,8 +114,6 @@ class Import(commands.Action):
         :return: "role" for "roleObject", "script" for "scriptObject"
         """
         return obj_type[:-6] if obj_type and obj_type.endswith("Object") else obj_type
-
-
 
     def _validate_object(self, yml):
         try:
@@ -129,11 +135,9 @@ class Import(commands.Action):
             return descr
 
         except (Exception, BaseException) as e:
-
             if settings.debug_mode:
                 click.echo(e)
                 click.echo(descr)
-
             raise click.ClickException("Invalid object description")
 
 
@@ -147,5 +151,5 @@ class ImportImage(commands.Action):
 
 
 class UpdateImage(commands.Action):
-    mutable_body_parts = ["name"]
-    prompt_for = ["imageId"]
+    mutable_body_parts = ['name']
+    prompt_for = ['imageId']
