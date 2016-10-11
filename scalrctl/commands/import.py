597d814f78cb5792732d9478e1db4516e2021894
Sergey Babak <s.babak@scalr.com>
2016-10-11 18:29:50 +0300

    search existing role categories

--- scalrctl/commands/import.py
+++ scalrctl/commands/import.py
@@ -7,7 +7,7 @@ import pydoc
 
 import yaml
 
-from scalrctl import click, commands, settings
+from scalrctl import click, commands, settings, utils
 
 
 __author__ = 'Dmitriy Korsakov'
@@ -36,11 +36,8 @@ class Import(commands.Action):
     }
 
     def _init(self):
+        self.scheme = utils.read_scheme()
         super(Import, self)._init()
-
-        with open(os.path.join(os.path.dirname(__file__),
-                               '../scheme/scheme.json')) as fp:
-            self.scheme = json.load(fp)
 
     def get_description(self):
         return "Import Scalr Objects."
@@ -95,13 +92,26 @@ class Import(commands.Action):
         import_objects = self._validate_object(raw_objects)
 
         for obj in import_objects:
+            action_name = obj['meta']['scalrctl']['ACTION']
+            obj_name = obj['data'].get('name')
+            result = None
+
             try:
-                obj = self._modify_object(obj)
-                result = self._import_object(obj, env_id, update_mode, dry_run)
+                if action_name == 'role-categories':
+                    # finds objects with the same names in new environment
+                    result = self._find_existing_object(action_name, obj_name, env_id)
+
+                if result:
+                    msg = "Warning: \"{}\" already exists\n".format(obj_name)
+                    click.secho(msg, bold=True, fg='yellow')
+                else:
+                    obj = self._modify_object(obj)  # updates object body with new ID's
+                    result = self._import_object(obj, env_id, update_mode, dry_run)
+
+                # save ID's of objects in new environment
                 self._save_imported(obj, result['data'])
             except Exception as e:
                 error_code = getattr(e, 'code', None)
-                action_name = obj['meta']['scalrctl']['ACTION']
                 ignored = ('role-categories', 'role-global-variables')
 
                 if error_code == 'UnicityViolation' and action_name in ignored:
@@ -110,13 +120,42 @@ class Import(commands.Action):
                     # TODO: delete imported objects
                     raise click.ClickException(str(e))
 
+    def _find_existing_object(self, action_name, obj_name, env_id):
+        """
+        Finds existing object with the same name in new environment.
+        """
+
+        list_data = self.scheme[action_name]['list']
+        list_action = commands.Action(
+            name=action_name,
+            route=list_data['route'],
+            http_method=list_data['http-method'],
+            api_level=list_data['api_level'],
+        )
+
+        list_kwargs = {
+            'envId': env_id,
+            'name': obj_name,
+            'hide_output': True
+        }
+
+        list_action_resp = list_action.run(**list_kwargs)
+        resp_data = json.loads(list_action_resp).get('data')
+
+        if resp_data and len(resp_data) > 0:
+            if len(resp_data) == 1:
+                return {'data': resp_data[0]}
+            else:
+                raise Exception("Matches for {} more than one!".format(action_name))
+
     def _import_object(self, obj_data, env_id, update_mode, dry_run=False):
         args, kwargs = obj_data['meta']['scalrctl']['ARGUMENTS']
         route = obj_data['meta']['scalrctl']['ROUTE']
         http_method = 'patch' if update_mode else 'post'
+        obj_name = obj_data['data'].get('name')
 
         action = None
-        for obj_name, section in self.scheme['export'].items():
+        for action_name, section in self.scheme['export'].items():
             if 'http-method' in section and 'route' in section and \
                             section['http-method'] == 'get' and \
                             section['route'] == route:
@@ -124,7 +163,7 @@ class Import(commands.Action):
                 cls = pydoc.locate(
                     scheme['class']
                 ) if 'class' in scheme else commands.Action
-                action = cls(name=obj_name, route=scheme['route'],
+                action = cls(name=action_name, route=scheme['route'],
                              http_method=http_method, api_level=self.api_level)
                 break
 
@@ -141,9 +180,10 @@ class Import(commands.Action):
         if env_id:
             kwargs['envId'] = env_id
 
-        click.secho("{} {} {}".format(
+        click.secho("{} {} {} {}...".format(
             "Updating" if update_mode else "Creating",
             self._get_object_alias(obj_type),
+            '\"%s\"' % obj_name if obj_name else '',
             "ID" if update_mode else ""
         ), bold=True)
 
